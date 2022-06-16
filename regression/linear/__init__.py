cc37ab16206fcb22fc8880ea543aba32e9f69c18
sixway <lw.2675@qq.com>
2022-06-16 20:52:14 +0800

    [feat] ordinary linear regression and it's test

---
+++ regression/linear/__init__.py
@@ -0,0 +1,4 @@
+#  Author: [sixwaaaay](https://github.com/sixwaaaay)
+
+from .ordinary import linear_regression
+from .ridge import ridge_regression
---
+++ tests/linear.py
@@ -0,0 +1,24 @@
+import pytest
+
+from regression.linear import linear_regression
+import numpy as np
+
+
+def test_linear_single_variable():
+    """
+    Test linear regression of single variable
+    assume that x is a 1D array and y is a 1D array
+    in this test, y = x + 4
+    """
+    x = np.array([
+        [1],
+        [2],
+        [3],
+        [4],
+        [5]])
+    # x were organized with row first, which means each row is a sample
+    # therefore, x is a 2D array
+    y = np.array([5, 6, 7, 8, 9])
+    reg = linear_regression(x, y)
+    assert reg.coef_[0] == pytest.approx(1)
+    assert reg.intercept_ == pytest.approx(4)
