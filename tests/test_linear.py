54ddf16c1e9fd2f933c86b3b87f625d2c3e58d4b
Restyled.io <commits@restyled.io>
2022-06-16 13:12:03 +0000

    Restyled by black

--- tests/test_linear.py
+++ tests/test_linear.py
@@ -10,12 +10,7 @@ def test_linear_single_variable():
     assume that x is a 1D array and y is a 1D array
     in this test, y = x + 4
     """
-    x = np.array([
-        [1],
-        [2],
-        [3],
-        [4],
-        [5]])
+    x = np.array([[1], [2], [3], [4], [5]])
     # x were organized with row first, which means each row is a sample
     # therefore, x is a 2D array
     y = np.array([5, 6, 7, 8, 9])
